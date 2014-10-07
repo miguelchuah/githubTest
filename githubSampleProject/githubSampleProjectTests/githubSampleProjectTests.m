fa95c7e6db7ce38460b7f52a65edccc162ff62dc
miguelchuah <mapplemig@gmail.com>
2014-10-08 00:12:17 +0800

    initial commit

---
+++ githubSampleProject/githubSampleProjectTests/githubSampleProjectTests.m
@@ -0,0 +1,40 @@
+//
+//  githubSampleProjectTests.m
+//  githubSampleProjectTests
+//
+//  Created by Miguel Nardy on 8/10/14.
+//  Copyright (c) 2014 tutorial. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface githubSampleProjectTests : XCTestCase
+
+@end
+
+@implementation githubSampleProjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
